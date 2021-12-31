d602bb3717e48199d78d53cf73fb75a7afb44b2f
Huda-cloud-97 <64609812+Huda-cloud-97@users.noreply.github.com>
2021-12-31 14:07:52 -0500

    First 4 chapters

---
+++ PS.py
@@ -0,0 +1,12 @@
+print ('Steve Jobs Rules')
+
+
+hrs = input("Enter Hours:")
+h = float(hrs)
+xx = input("Enter the Rate:")
+x = float(xx)
+if h <= 40:
+ 	print( h  * x)
+elif h > 40:
+	print(40* x + (h-40)*1.5*x)
+    
