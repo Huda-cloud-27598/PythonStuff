8310f50235568acc9b1ea38582b88db4389954ea
Huda-cloud-97 <64609812+Huda-cloud-97@users.noreply.github.com>
2022-02-28 20:28:47 -0500

    exersise 7

---
+++ ex7/7_1.py
@@ -0,0 +1,16 @@
+# 7.1 Write a program that prompts for a file name,
+# then opens that file and reads through the file,
+# and print the contents of the file in upper case. Use the file words.txt
+# to produce the output below.
+# Use words.txt as the file name
+
+
+fname = input("Enter file name: ")
+try:
+    fh = open(fname)
+except:
+    print('Cannot open the file ',fh ,'please try again')
+    quit()
+for indo in fh:
+    ind = indo.rstrip()
+    print(ind.upper())
